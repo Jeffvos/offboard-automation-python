cdf13346a21d61e590d44a707972eeaf1e942512
Jeffrey Vos <jeff.vos@aol.com>
2020-01-29 11:19:11 +0200

    refactored gitlab service

--- gitlabservice.py
+++ gitlabservice.py
@@ -1,36 +1,34 @@
-import requests
-import time
+""" Gitlab service module """
 import json
-from requests.auth import HTTPBasicAuth
+import requests
 import jiraservice
 
 with open('appconfig.json') as f:
-    appconfig=json.load(f)
+    APPCONFIG = json.load(f)
 
-gitbaseUrl = appconfig['gitlab']['base_url']
-githeaders={"Private-Token": appconfig['gitlab']['token']}
+GIT_BASE_URL = APPCONFIG['gitlab']['base_url']
+GIT_HEADER = {"Private-Token": APPCONFIG['gitlab']['token']}
 
-def block_user(usersToBlock, jiraIssue):
-    errorList=[]
-    for user in usersToBlock[0]:
-        userUrl = gitbaseUrl+"users?username={}".format(user)
-        userRequest = requests.get(userUrl, headers=githeaders)
-        userData = userRequest.json()
-        print(userData)
+def block_user(users_to_block, jira_issue):
+    """ Function to check the user and block user with second request """
+    error_list = []
+    for user in users_to_block[0]:
+        git_user_url = GIT_BASE_URL+"users?username={}".format(user)
+        user_req = requests.get(git_user_url, headers=GIT_HEADER)
+        user_data = user_req.json()
         try:
-            if (userData=='[]'):
-                errorList.append(user)
+            if user_data == '[]':
+                error_list.append(user)
             else:
-                print('Found: '+ user)
-                userID=userData[0]['id']
-                blockUrl= gitbaseUrl+"users/{}/block".format(userID)
-                blockRequest = requests.post(blockUrl, headers=githeaders)
-                print(blockRequest)
-        except:
+                user_id = user_data[0]['id']
+                git_block_url = GIT_BASE_URL+"users/{}/block".format(user_id)
+                block_req = requests.post(git_block_url, headers=GIT_HEADER)
+                print(block_req)
+        except IndexError:
             print('error')
-            errorList.append(user)
-    if errorList != []:
-        print(errorList)
+            error_list.append(user)
+    if error_list != []:
+        print(error_list)
     else:
-        jiraservice.updateticket(jiraIssue)
-    return
+        jiraservice.updateticket(jira_issue)
+    return True
