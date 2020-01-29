a49fad34dfa24c58279dc6b20c61c6278fcba8cb
Jeffrey Vos <jeff.vos@aol.com>
2020-01-29 11:10:20 +0200

    refactored jira service

--- jiraservice.py
+++ jiraservice.py
@@ -1,71 +1,83 @@
-import requests
-import time
+"""Jira Service Module"""
+import sys
 import json
-from requests.auth import HTTPBasicAuth
+import requests
 
 with open('appconfig.json') as f:
-    appconfig=json.load(f)
+    APPCONFIG = json.load(f)
 
-jirabaseUrl = appconfig['jira']['base_url']
-jiraheaders = {"content-type": "application/json"}
-jiraAuth=(appconfig['jira']['basic_auth']['username'],appconfig['jira']['basic_auth']['password'])
+JIRA_BASE_URL = APPCONFIG['jira']['base_url']
+JIRA_HEADERS = {"content-type": "application/json"}
+JIRA_USER = APPCONFIG['jira']['basic_auth']['username']
+JIRA_PASS = APPCONFIG['jira']['basic_auth']['password']
+JIRA_AUTH = (JIRA_USER, JIRA_PASS)
 
 def check_issues():
-    usersToBlock=[]
-    #check for open jira offboarding issues based on JQL
-    jiraUrl = jirabaseUrl+appconfig['jira']['jqlurl']['git-ext']
-    jiraRequest= requests.get(jiraUrl, auth=jiraAuth, headers=jiraheaders)
-    jsonRes=jiraRequest.json()
+    """ Function to check if there is a issue in jira based on jql """
+    users_to_block = []
+    jira_url = JIRA_BASE_URL+APPCONFIG['jira']['jqlurl']['git-ext']
+    jira_request = requests.get(jira_url, auth=JIRA_AUTH, headers=JIRA_HEADERS)
+    json_res = jira_request.json()
     #for now expecting one issue if none exit script
     try:
-        print(len(jsonRes['issues']))
-        jiraIssue=jsonRes['issues'][0]['key']
-    except:
+        print(len(json_res['issues']))
+        jira_issue = json_res['issues'][0]['key']
+    except IndexError:
         print('No new issues found.')
-        exit()
-    content = jsonRes['issues'][0]['fields']['description']
-    contentsplit =content.split()
-    #still need to add multiple check to add to usersToBlock list
+        sys.exit()
+    content = json_res['issues'][0]['fields']['description']
+    contentsplit = content.split()
+    print(content)
     for items in contentsplit:
-        outputm=items.find('@')
-        if(outputm == -1):
-            continue
-        else:
-            usersToBlock.append(items[:outputm])
-    print(usersToBlock)
-    return usersToBlock, jiraIssue
+        outputm = items.find('@')
+        if outputm != -1:
+            users_to_block.append(items[:outputm])
+    print(users_to_block)
+    return users_to_block, jira_issue
 
-def block_user(usersToBlock):
-    jiraData="{\"active\": \"false\"}"
-    succount=0
-    failcount=0
-    errorList=[]
-    for user in usersToBlock[0]:
-        jiraBlock = baseUrl+"user?username={}".format(user)
-        jiraRequest= requests.put(jiraBlock, data=jiraData, auth=jiraAuth, headers=jiraheaders)
-        jsonRes=jiraRequest.json()
-        #check if transaction was successful
+def block_user(users_to_block):
+    """ Function to change the users status to false """
+    jira_data = "{\"active\": \"false\"}"
+    suc_count = 0
+    fail_count = 0
+    error_list = []
+    for user in users_to_block[0]:
+        jira_block = JIRA_BASE_URL+"user?username={}".format(user)
+        jira_req = requests.put(jira_block, data=jira_data, auth=JIRA_AUTH, headers=JIRA_HEADERS)
+        jira_json = jira_req.json()
         try:
-            if(jsonRes['errors'] != {}):
-                print(jsonRes['errors'])
-                errorList.append(user)
-                failcount = failcount + 1
-        except:
+            if jira_json['errors'] != {}:
+                print(jira_json['errors'])
+                error_list.append(user)
+                fail_count = fail_count + 1
+        except IndexError:
             print('Deactivated: '+ user)
-            succount = succount + 1
+            suc_count = suc_count + 1
 
-    print('Users deactivated: {}. Failed deactivation {}.'.format(succount, failcount))
+    print('Users deactivated: {}. Failed deactivation {}.'.format(suc_count, fail_count))
 
-    if failcount >= 1:
+    if fail_count >= 1:
         print('Failed to deactivate users: ')
-        print(errorList)
+        print(error_list)
     else:
-        updateticket(usersToBlock[1])
-    return
+        updateticket(users_to_block[1])
+    return True
 
 def updateticket(jirakey):
+    """ Function to update jira ticket for transistion id please use the workflows close ID"""
     print("updating ticket "+ jirakey)
-    comment = appconfig['jira']['comment']
-    jiraCommentUrl= jirabaseUrl+"issue/"+jirakey+"/transitions"
-    commentRequest = requests.post(jiraCommentUrl, data=comment, auth=jiraAuth, headers=jiraheaders)
-    return
+    #comment = appconfig['jira']['comment']
+    comment = """{
+                "update":{
+                    "comment":[{"add": {"body": "All Users are deactivated."}}]
+                },
+                "transition":{
+                    "id": "171"
+                    }
+                }
+            """
+    deco = json.loads(comment)
+    newcom = json.dumps(deco)
+    jira_comment = JIRA_BASE_URL+"issue/"+jirakey+"/transitions"
+    c_request = requests.post(jira_comment, data=newcom, auth=JIRA_AUTH, headers=JIRA_HEADERS)
+    return c_request
