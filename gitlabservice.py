e3444b136e4d23a1dbdd664fa564d5096716a584
Jeffrey Vos <jeff.vos@aol.com>
2020-02-10 10:00:46 +0200

    additional changes to gitservice

--- gitlabservice.py
+++ gitlabservice.py
@@ -6,28 +6,41 @@ import jiraservice
 with open('appconfig.json') as f:
     APPCONFIG = json.load(f)
 
-GIT_BASE_URL = APPCONFIG['gitlab']['base_url']
-GIT_HEADER = {"Private-Token": APPCONFIG['gitlab']['token']}
 
-def block_user(users_to_block, jira_issue):
-    """ Function to check the user and block user with second request """
-    error_list = []
-    for user in users_to_block[0]:
-        git_user_url = GIT_BASE_URL+"users?username={}".format(user)
-        user_req = requests.get(git_user_url, headers=GIT_HEADER)
-        user_data = user_req.json()
-        try:
-            if user_data == '[]':
-                error_list.append(user)
-            else:
-                user_id = user_data[0]['id']
-                git_block_url = GIT_BASE_URL+"users/{}/block".format(user_id)
-                block_req = requests.post(git_block_url, headers=GIT_HEADER)
-                print(block_req)
-        except IndexError:
-            error_list.append(user)
-    if error_list != []:
-        print(error_list)
-    else:
-        jiraservice.updateticket(jira_issue)
-    return True
+class Gitlab():
+    def __init__(self):
+        self._base_url = APPCONFIG['gitlab']['base_url']
+        self._header = {"Private-Token": APPCONFIG['gitlab']['token']}
+        self._success = []
+        self._failded = []
+
+    def _compose_url(self, action):
+        """ compose url based on action """
+        options = {"check_user": "users?username={}",
+                   "disable_user": "users/{}/block"}
+        return self._base_url+options[action]
+
+    def _success_dis(self, user):
+        print(user)
+
+    def _request(self, method, url, user):
+        """ request depending on method and url"""
+        req = requests.request(method, url, headers=self._header)
+        print(req)
+        print(method)
+        if method == "GET":
+            data_json = req.json()
+            user_id = data_json[0]['id']
+            return self._disable_user(user_id)
+        else:
+
+            return self._success_dis(user)
+
+    def check_user(self, user):
+        url = self._compose_url("check_user").format(user)
+        return self._request("GET", url, user)
+
+    def _disable_user(self, user_id):
+        url = self._compose_url("disable_user").format(user_id)
+        print(url)
+        return self._request("POST", url, user_id)
