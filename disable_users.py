456372857a338a89d2b46cdef7b084699272ce4c
Jeffrey Vos <jeff.vos@aol.com>
2020-02-17 09:31:42 +0200

    added loop to check envs in json

--- disable_users.py
+++ disable_users.py
@@ -1,6 +1,15 @@
 """ Main - still work in progress """
+import json
 import gitlabservice
 import jiraservice
 
+with open('appconfig.json') as f:
+    APPCONFIG = json.load(f)
 
-jiraservice.Jira('jira').call()
+
+def disable_users():
+    for i in APPCONFIG['jira']['jqlurl']:
+        jiraservice.Jira(i).call()
+
+
+disable_users()
